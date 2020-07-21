6373c72e19e17226d836ea213a401ba275c95e6a
Stefano <stefano.zaccaria29@gmail.com>
2020-07-21 16:32:19 +0200

    ex complete!

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-// GOAL: Griglia 6x6, ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9. Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde. Il numero ottenuto appare al centro del quadrato.
-function init() {
-    
+function init() {    
     $('#btn').click(startToPlay);
-
 }
+
+//Funzione per il gioco GOAL: ad ogni click dall'url della chiamata ajax riceviamo un numero random. A seconda del valore cambia il colore del div nella griglia
 
 function startToPlay() {
     var divActive = $('#grid>div.active');
-
     $.ajax({
         url : 'https://flynn.boolean.careers/exercises/api/random/int',
         method : 'GET',
         success : function (data,state) {
-
             var response = data.response;
             var success = data.success;
-            console.log(data);
             if (success) {
                     if (response<= 5) {
-                        divActive.append(response);
-                        divActive.addClass('yellow');
+                        divActive.append(response).addClass('yellow');
                     } else {
-                        divActive.append(response);
-                        divActive.addClass('green');
+                        divActive.append(response).addClass('green');
                     }
             } else {
                 console.log('error');
@@ -35,9 +29,7 @@ function startToPlay() {
             console.log(error);
         }
     })
-    
     divActive.removeClass('active');
     divActive.next().addClass('active');
 }
-
 $(document).ready(init);
